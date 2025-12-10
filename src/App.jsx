c00b01a76f6c225afa044a0803416b37aef951b9
Harshita <harshitagupta1377@gmail.com>
2025-12-10 20:56:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl text-green-800'>Netflix-GPT</h1>
+    </>
+  )
+}
+
+export default App
